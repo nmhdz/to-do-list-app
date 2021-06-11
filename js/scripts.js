7ab60043c1844caaa90f7688ccde74f9837ddd3b
nmhdz <nmontana.cf@gmail.com>
2021-06-11 17:04:48 -0600

    Update scripts.js
    created a function that will cross out an item from the list of items. Also added the delete button and made it possible to reorder the items.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,29 @@ function newItem(){
   } else {
     $('#list').append(li);
       }
+
+// Crossing out an item from the list of items:
+
+  function crossOut() {
+    li.toggleClass('strike');
+      }
+
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+// Adding the delete button "X":
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+  function deleteListItem(){
+    li.addClass('delete')
+  }
+
+// Reordering the items:
+
+  $('#list').sortable();
+}
